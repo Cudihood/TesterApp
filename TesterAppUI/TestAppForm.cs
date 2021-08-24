705d5d5b9fcd961e02210f14b61925f5003a17e6
Даниил Циркунов <danilmordovin@mail.ru>
2021-08-24 22:28:05 +0700

    добавлены обработки исключений

--- TesterAppUI/TestAppForm.cs
+++ TesterAppUI/TestAppForm.cs
@@ -38,8 +38,109 @@ namespace TesterAppUI
             PortsComboBox.Items.Clear();
             //Добавление найденных портов в бокс
             PortsComboBox.Items.AddRange(ports);
+            if (PortsComboBox.Items.Count == 0)
+            {
+                MessageBox.Show("Нет доступных портов.", "Внимание!", MessageBoxButtons.OK);
+            }
+            else
+            {
+                PortsComboBox.SelectedIndex = 0;
+            }
+            BaudRateComboBox.SelectedIndex = 3;
+            DataBitsComboBox.SelectedIndex = 1;
+            ParityComboBox.SelectedIndex = 0;
+            StopBitsСomboBox.SelectedIndex = 0;
         }
 
+        /// <summary>
+        /// Возвращает и задает адрес устройства
+        /// </summary>
+        public byte SlaveAddress
+        {
+            get
+            {
+                return slaveAddress;
+            }
+
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentException("Пустая строка");
+                }
+
+                if (value < 0 || value > 255) 
+                {
+                    throw new ArgumentException("Адрес устройства может принимать значение от 0 до 255"); 
+                }
+                else
+                {
+                    slaveAddress = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задет адрес регистра
+        /// </summary>
+        public ushort StartAddress
+        {
+            get
+            {
+                return startAddress;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentException("Пустая строка");
+                }
+
+                if (value < 0 || value > 0xFFFF) 
+                {
+                    throw new ArgumentException("Адрес регистра может принимать значение от 0 до FFFF");
+                }
+                else
+                {
+                    startAddress = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задает количество регистров 
+        /// </summary>
+        public ushort NumberOfPoints
+        {
+            get
+            {
+                return numberOfPoints;
+            }
+
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentException("Пустая строка");
+                }
+
+                if (value > 9999 || value < 1) 
+                {
+                    throw new ArgumentException("Неверное количество регистров.\nМаксимальное количество: 9999.");
+                }
+                
+                else
+                {
+                    numberOfPoints = value;
+                }
+            }
+        }
+        
+        /// <summary>
+        /// Открывет порт
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void OpenButton_Click(object sender, EventArgs e)
         {
             /*
@@ -51,12 +152,37 @@ namespace TesterAppUI
             */
             try
             {
-                port = new SerialPort(PortsComboBox.Text, 9600, Parity.None, 8, StopBits.One);
+                port = new SerialPort();
+                port.PortName = PortsComboBox.Text;
+                port.BaudRate = Convert.ToInt32(BaudRateComboBox.Text);
+
+                if (ParityComboBox.Text.Substring(0, 1) == "0")
+                {
+                    port.Parity = Parity.None;
+                }
+                else if (ParityComboBox.Text.Substring(0, 1) == "1")
+                {
+                    port.Parity = Parity.Odd;
+                }
+                else if (ParityComboBox.Text.Substring(0, 1) == "2")
+                {
+                    port.Parity = Parity.Even;
+                }
+
+                if (StopBitsСomboBox.Text == "0")
+                {
+                    port.StopBits = StopBits.One;
+                }
+                else if(StopBitsСomboBox.Text == "1")
+                {
+                    port.StopBits = StopBits.Two;
+                }
+                
                 port.Open();
 
-                slaveAddress = Convert.ToByte(SlaveAddressTextBox.Text);
-                startAddress = Convert.ToUInt16(StartAddresTextBox.Text, 16);
-                numberOfPoints = Convert.ToUInt16(NumberOfPointsTextBox.Text);
+                SlaveAddress = Convert.ToByte(SlaveAddressTextBox.Text);
+                StartAddress = Convert.ToUInt16(StartAddressTextBox.Text, 16);
+                NumberOfPoints = Convert.ToUInt16(NumberOfPointsTextBox.Text);
 
                 masrerRTU = ModbusSerialMaster.CreateRtu(port); 
 
@@ -64,18 +190,35 @@ namespace TesterAppUI
                 CloseButton.Enabled = true;
                 
             }
+            catch (ArgumentException exception)
+            {
+                MessageBox.Show($"{exception.Message}", "Ошибка", MessageBoxButtons.OK);
+                return;
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Строка должна содержать только цифры.", "Ошибка", MessageBoxButtons.OK);
+                return;
+            }
             catch (Exception ex)
             {
-                MessageBox.Show(this, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show(this, ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
             }
+
         }
 
+        /// <summary>
+        /// Получение данных с регистра
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void GetButton_Click(object sender, EventArgs e)
         {
             try
             {
-                startAddress = Convert.ToUInt16(StartAddresTextBox.Text, 16);
-                ushort[] result = masrerRTU.ReadHoldingRegisters(slaveAddress, startAddress, numberOfPoints);
+                StartAddress = Convert.ToUInt16(StartAddressTextBox.Text, 16);
+                ushort[] result = masrerRTU.ReadHoldingRegisters(SlaveAddress, StartAddress, NumberOfPoints);
                 GetTextBox.Text = string.Empty;
                 foreach (ushort item in result)
                 {
@@ -110,6 +253,11 @@ namespace TesterAppUI
             */
         }
 
+        /// <summary>
+        /// Закрывает порт
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void CloseButton_Click(object sender, EventArgs e)
         {
             //objModbusRtuProtocol.Port.Close();
@@ -118,17 +266,67 @@ namespace TesterAppUI
             CloseButton.Enabled = false;
         }
 
+        /// <summary>
+        /// Запись данных в регистр 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void SetButton_Click(object sender, EventArgs e)
         {
             try
             {
-                startAddress = Convert.ToUInt16(StartAddresTextBox.Text, 16);
+                StartAddress = Convert.ToUInt16(StartAddressTextBox.Text, 16);
                 ushort value = Convert.ToUInt16(SetTextBox.Text);
-                masrerRTU.WriteSingleRegister(slaveAddress, startAddress, value);
+                masrerRTU.WriteSingleRegister(SlaveAddress, StartAddress, value);
             }
             catch (Exception ex)
             {
                 MessageBox.Show(this, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// Проверка на ввод адреса устройства
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SlaveAddressTextBox_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                SlaveAddress = Convert.ToByte(SlaveAddressTextBox.Text);
+                SlaveAddressTextBox.BackColor=Color.White;
+            }
+            catch
+            {
+                SlaveAddressTextBox.BackColor = Color.LightPink;
+            }
+            
+        }
+
+        private void StartAddresTextBox_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                StartAddress = Convert.ToUInt16(StartAddressTextBox.Text, 16);
+                StartAddressTextBox.BackColor = Color.White;
+            }
+            catch
+            {
+                StartAddressTextBox.BackColor = Color.LightPink;
+            }
+        }
+
+        private void NumberOfPointsTextBox_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                NumberOfPoints = Convert.ToUInt16(NumberOfPointsTextBox.Text, 16);
+                NumberOfPointsTextBox.BackColor = Color.White;
+            }
+            catch
+            {
+                NumberOfPointsTextBox.BackColor = Color.LightPink;
             }
         }
     }
